425d405f782d9906cfca6db9d9b8cd051e97aef3
Eudes Silva <eudessilvabezerra985@gmail.com>
2025-06-08 16:44:08 -0300

    new versions

--- core/models/grade.py
+++ core/models/grade.py
@@ -2,11 +2,18 @@
 
 from django.db import models
 
+from .usuario import User
 
+from .subject import Subject
+
+CHOICES = sorted([("1","1ª unidade "),("2","2ª unidade"),("3","3ª unidade ")], key= lambda x:x[1])
 class Grade(models.Model):
-
-    nota = models.FloatField
-    unidade = models.PositiveIntegerField
+    
+    unidade = models.CharField(max_length=1,choices=CHOICES)
+    nota = models.FloatField()
+    disciplina = models.ForeignKey(Subject, on_delete=models.CASCADE,related_name='notas')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,related_name='usuario')
+    
 
     def __str__(self) -> str:
-        return super().__str__()
+        return self.unidade
--- core/models/subject.py
+++ core/models/subject.py
@@ -2,10 +2,15 @@
 from django.db import models
 from django.forms import ModelForm
 
+from .usuario import User
+
+
+HORARIO_CHOICES = sorted([ (15,"15HRS"),(30, "30HRS"),(45,"45HRS"),(60,"60HRS"),(90,"90HRS")], key = lambda x:x[1])
 class Subject(models.Model):
     name = models.CharField(max_length=40)
-    code = models.IntegerField(default= 0)
-    horario = models.CharField(max_length=10)
+    codigo = models.CharField(max_length=10,unique=True,)
+    carga_horaria = models.IntegerField(null=False,choices=HORARIO_CHOICES)
+    alunos = models.ManyToManyField(User, related_name='disciplinas')
 
     def __str__(self) -> str:
         return self.name
