c56fda8d1cfd09606a79059047eea9e2e8b8aa75
Eudes Silva <eudessilvabezerra985@gmail.com>
2025-06-21 23:45:31 -0300

    Nova versão ajustada

--- core/models/subject.py
+++ core/models/subject.py
@@ -5,11 +5,13 @@ from django.forms import ModelForm
 from .usuario import User
 
 
-HORARIO_CHOICES = sorted([ (15,"15HRS"),(30, "30HRS"),(45,"45HRS"),(60,"60HRS"),(90,"90HRS")], key = lambda x:x[1])
-class Subject(models.Model):
+HORARIO_CHOICES = sorted([(30, "30HRS"),(60,"60HRS"),(90,"90HRS")], key = lambda x:x[1])
+SEMESTRE_CHOICES = sorted([("2025.1","2025.1"),("2024.2","2024.2"),("2024.2","2024.2"),("2024.1","2024.1"),("2023.2","2023.2"),("2023.1","2023.1"),("2022.2","2022.2"),("2022.1","2022.1")] ,key=lambda x:x[-1] )
+class Subject(models.Model):    
     name = models.CharField(max_length=40)
-    codigo = models.CharField(max_length=10,unique=True,)
+    codigo = models.CharField(max_length=10,unique=True)
     carga_horaria = models.IntegerField(null=False,choices=HORARIO_CHOICES)
+    semestre = models.CharField(choices=SEMESTRE_CHOICES,default="2025.1")
     alunos = models.ManyToManyField(User, related_name='disciplinas')
 
     def __str__(self) -> str:
